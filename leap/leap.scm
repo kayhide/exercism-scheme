80c82ccad24ad4216a2fc14789459438a3c3c8ca
Hideaki Kawai <kayhide@gmail.com>
2020-05-16 22:26:28 +0900

    Solve leap

---
+++ leap/leap.scm
@@ -0,0 +1,12 @@
+(import (rnrs))
+
+(define (is-divisible? x y)
+  (zero? (mod x y)))
+
+(define (leap-year? year)
+  (cond
+   ((is-divisible? year 400) #t)
+   ((is-divisible? year 100) #f)
+   ((is-divisible? year 4) #t)
+   (#t #f)
+   ))
